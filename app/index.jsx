28ef026002492f244856fdea79c88be65e45b4a2
gabaish <gabay168@gmail.com>
2024-10-27 21:51:34 +0200

    remove duplicates

--- app/index.jsx
+++
@@ -1,6 +0,0 @@
-import { Redirect } from 'expo-router';
-
-export default function Index() {
-  // Automatically redirect to the onboarding flow when the app starts
-  return <Redirect href="/(onboarding)/onboardingNavigator" />;
-}
